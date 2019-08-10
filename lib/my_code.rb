0ca7f56ae15d0b9f507bf8376364290943881b5a
Hector Rodriguez Duenas <h.rodri831@gmail.com>
2019-08-10 02:53:16 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,52 @@
 # My Code here....
+def map_to_negativize(array)
+  array.length.times { |index| 
+    array[index] = array[index] * -1
+  }
+  return array
+end
+
+def map_to_no_change(array)
+  array
+end
+
+def map_to_double(array)
+  array.length.times { |index| 
+    array[index] = array[index] * 2
+  }
+  return array
+end
+
+def map_to_square(array)
+  array.length.times { |index| 
+    array[index] = array[index] ** 2 
+  }
+  return array
+end
+
+def reduce_to_total(array, starting = 0)
+  array.length.times { |index| 
+    starting = starting + array[index] 
+  }
+  starting
+end
+
+def reduce_to_all_true(array)
+  solution = nil
+  index = 0
+    while index <= array.length  
+      if array[index] = false
+        solution = array[index]
+      end
+      if array[index] = true
+        solution = array[index]
+      end
+      index += 1
+    end
+  return solution
+end
+
+def reduce_to_any_true(array)
+  solution = nil
+  return solution
+end
